05f7ab208835148476f57b60d052cf5d712d3792
Shrey <ShreyasJothish>
2019-08-01 07:43:54 -0700

    Completed functionality of adding to base recommendation db

--- scripts/basersload.py
+++ scripts/basersload.py
@@ -1,21 +1,113 @@
+import datetime
 import googlemaps
+from gql import Client, gql
+from gql.transport.requests import RequestsHTTPTransport
+import json
 import os
+import pytz
 
 # Environment variables
 GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+GRAPHQL_URL = os.environ['GRAPHQL_URL']
 
-# Initializing client
+# Initializing google client
 gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
+
+# Initializing graphql client
+expected_retries = 3
+gql_client = Client(retries=expected_retries,
+                    transport=RequestsHTTPTransport(url=GRAPHQL_URL))
+
+
+def get_utc_time():
+    """
+    Returns UTC time in ISO format
+
+    Output format.
+    'YYYY-mm-ddTHH:MM:SS+HH:MM'
+
+    '2019-08-01T10:43:30+00:00'
+    """
+    date = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+    date = datetime.datetime(date.year, date.month, date.day,
+                             date.hour, date.minute, int(date.second),
+                             int(date.second * 1000000 % 1000000),
+                             tzinfo=date.tzinfo)
+
+    return date.isoformat()
 
 
 def find_place(user_input):
+    """
+    Find the place using Google Place API.
+    To get more accurate or specific place information, provide more details
+    as part of input.
+
+    Input: User provided place name
+    Output: 0 or more locations matching the given name.
+    """
     my_fields = ['name', 'photos', 'place_id', 'types', 'formatted_address']
     place_results = gmaps.places(query=user_input)
     return place_results
 
 
 def fetch_place_details(place_id):
+    """
+    Fetch place details using Google Place API.
+
+    Input: Place ID, unique identifier obtained as part of Google Place API
+    based on user input.
+    Output: Detailed information about the place as provided by Google Place
+    API.
+    """
     return gmaps.place(place_id)
+
+
+def add_to_basers_db(place_id, place_details):
+    """
+    Add Place details obtained from Google Place API into local DB.
+    This is done using GraphQL API for storing Base Recommendation System
+    information.
+
+    Input:
+    1. Place ID
+    2. Google Place Details
+    """
+    place_details_str = json.dumps(place_details)
+
+    # Below clean up is needed to ensure the json data is in correct format
+    # to be stored using GraphQL API.
+    temp_place_details_str = place_details_str.replace('\\\"', "'")
+    updated_place_details_str = temp_place_details_str.replace('"', '\\"')
+
+    # GraphQL API for adding Place inforamation to be used for Base
+    # Recommendation System.
+    createBaseRSPlace = gql('''
+    mutation createBaseRSPlace($place_id: String!,
+                               $place_details: JSONString!,
+                               $timestamp: CustomGrapheneDateTime!) {
+      create_basers_place(place_data: {place_id: $place_id,
+                                       google_place_details: $place_details,
+                                       timestamp: $timestamp}) {
+        place_id
+        ok
+        message
+      }
+    }
+    ''')
+
+    place_details = updated_place_details_str
+    timestamp = get_utc_time()
+
+    # Creating dynamic parameter list
+    place_id_str = f'    "place_id": "{place_id}",\n'  # noqa: E999
+    place_details_str = f'    "place_details": "{place_details}",\n'
+    timestamp_str = f'    "timestamp": "{timestamp}"\n'
+    params = "{\n"+place_id_str+place_details_str+timestamp_str+"}"
+
+    print(params)
+
+    return gql_client.execute(createBaseRSPlace, variable_values=params)
 
 
 class color:
@@ -119,6 +211,9 @@ if __name__ == "__main__":
             print(f"Failed to fetch details for {name}.")
             continue
 
-        place_details_result = place_details_response['result']
+        place_details = place_details_response['result']
 
-        print(place_details_result)
+        try:
+            add_to_basers_db(place_id, place_details)
+        except Exception:
+            print("Failed to add place details into local DB")
